181d15fa6573699120cd06526076a986365c3dba
Artur <art67789@gmail.com>
2025-04-04 22:45:35 +0300

    Implement handler to get single user

--- api/user_handler.go
+++ api/user_handler.go
@@ -1,7 +1,31 @@
 package api
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/qppffod/reservation-api/db"
+)
 
-func HandleGetUser(c *fiber.Ctx) error {
-	return c.JSON("single user")
+type UserHandler struct {
+	userStore db.UserStore
+}
+
+func NewUserHandler(userStore db.UserStore) *UserHandler {
+	return &UserHandler{
+		userStore: userStore,
+	}
+}
+
+func (h *UserHandler) HandleGetUser(c *fiber.Ctx) error {
+	id := c.Params("id")
+
+	user, err := h.userStore.GetUserByID(c.Context(), id)
+	if err != nil {
+		return err
+	}
+
+	return c.JSON(user)
+}
+
+func (h *UserHandler) HandleGetUsers(c *fiber.Ctx) error {
+	return c.JSON("many users")
 }
