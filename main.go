22e7b2111a33858df08191e12538b9cc680bcdb8
Artur <art67789@gmail.com>
2025-04-04 22:03:47 +0300

    Add mongodb connection

--- main.go
+++ main.go
@@ -1,16 +1,33 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"fmt"
 	"log"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/qppffod/reservation-api/api"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
+var dburi = "mongodb://localhost:27017"
 
 func main() {
 	listenAddr := flag.String("listenAddr", ":3000", "listen address of http server")
 	flag.Parse()
+
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(dburi))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := client.Ping(context.TODO(), nil); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(client)
 
 	app := fiber.New()
 	apiv1 := app.Group("/api/v1")
