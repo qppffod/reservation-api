5432890170d1993f964d31f4f59618a173ce1790
Artur <art67789@gmail.com>
2025-04-04 23:04:28 +0300

    Add type for POST request to user handler

--- types/user.go
+++ types/user.go
@@ -1,11 +1,35 @@
 package types
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type CreateUserParams struct {
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+	Email     string `json:"email"`
+	Password  string `json:"password"`
+}
 
 type User struct {
 	ID                primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
 	FirstName         string             `bson:"firstName" json:"firstName"`
 	LastName          string             `bson:"lastName" json:"lastName"`
 	Email             string             `bson:"email" json:"email"`
-	EncryptedPassword string             `bson:"encryptedPassword" json:"encryptedPassword"`
+	EncryptedPassword string             `bson:"encryptedPassword" json:"-"`
+}
+
+func NewUserFromParams(user *CreateUserParams) (*User, error) {
+	encpw, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, err
+	}
+
+	return &User{
+		FirstName:         user.FirstName,
+		LastName:          user.LastName,
+		Email:             user.Email,
+		EncryptedPassword: string(encpw),
+	}, nil
 }
