317ab1b5ab7b444346aa98672f264453168a18a9
Artur <art67789@gmail.com>
2025-04-05 20:28:13 +0300

    Change room type

--- types/hotel.go
+++ types/hotel.go
@@ -10,19 +10,10 @@ type Hotel struct {
 	Rating   int                  `bson:"rating" json:"rating"`
 }
 
-type RoomType int
-
-const (
-	SingleRoomType RoomType = iota + 1
-	DoubleRoomType
-	SeaSideRoomType
-	DeluxeRoomType
-)
-
 type Room struct {
-	ID        primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
-	Type      RoomType           `bson:"type" json:"type"`
-	BasePrice float64            `bson:"basePrice" json:"basePrice"`
-	Price     float64            `bson:"price" json:"price"`
-	HotelID   primitive.ObjectID `bson:"hotelID" json:"hotelID"`
+	ID      primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
+	Size    string             `bson:"size" json:"size"` // small, normal, large
+	Seaside bool               `bson:"seaside", json:"seaside"`
+	Price   float64            `bson:"price" json:"price"`
+	HotelID primitive.ObjectID `bson:"hotelID" json:"hotelID"`
 }
