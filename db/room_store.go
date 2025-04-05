4ebf33ffd0862480041779931cac290429b976f0
Artur <art67789@gmail.com>
2025-04-05 18:54:36 +0300

    Room mongo store, implemented insert operation

---
+++ db/room_store.go
@@ -0,0 +1,43 @@
+package db
+
+import (
+	"context"
+
+	"github.com/qppffod/reservation-api/types"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type RoomStore interface {
+	InsertRoom(context.Context, *types.Room) (*types.Room, error)
+}
+
+type MongoRoomStore struct {
+	client *mongo.Client
+	coll   *mongo.Collection
+
+	hotelStore HotelStore
+}
+
+func NewMongoRoomStore(client *mongo.Client, dbname string, hotelStore HotelStore) *MongoRoomStore {
+	return &MongoRoomStore{
+		client:     client,
+		coll:       client.Database(dbname).Collection("rooms"),
+		hotelStore: hotelStore,
+	}
+}
+
+func (s *MongoRoomStore) InsertRoom(ctx context.Context, room *types.Room) (*types.Room, error) {
+	res, err := s.coll.InsertOne(ctx, room)
+	if err != nil {
+		return nil, err
+	}
+
+	room.ID = res.InsertedID.(primitive.ObjectID)
+
+	if err := s.hotelStore.UpdateHotel(ctx, room); err != nil {
+		return nil, err
+	}
+
+	return room, nil
+}
