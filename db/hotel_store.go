06a00881ff9bea36f267a32846263546c78581ca
Artur <art67789@gmail.com>
2025-04-05 18:54:01 +0300

    Hotel mongo store, implemented insert and update operations

---
+++ db/hotel_store.go
@@ -0,0 +1,45 @@
+package db
+
+import (
+	"context"
+
+	"github.com/qppffod/reservation-api/types"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type HotelStore interface {
+	InsertHotel(context.Context, *types.Hotel) (*types.Hotel, error)
+	UpdateHotel(context.Context, *types.Room) error
+}
+
+type MongoHotelStore struct {
+	client *mongo.Client
+	coll   *mongo.Collection
+}
+
+func NewMongoHotelStore(client *mongo.Client, dbname string) *MongoHotelStore {
+	return &MongoHotelStore{
+		client: client,
+		coll:   client.Database(dbname).Collection("hotels"),
+	}
+}
+
+func (s *MongoHotelStore) InsertHotel(ctx context.Context, hotel *types.Hotel) (*types.Hotel, error) {
+	res, err := s.coll.InsertOne(ctx, hotel)
+	if err != nil {
+		return nil, err
+	}
+
+	hotel.ID = res.InsertedID.(primitive.ObjectID)
+
+	return hotel, nil
+}
+
+func (s *MongoHotelStore) UpdateHotel(ctx context.Context, room *types.Room) error {
+	update := bson.M{"$push": bson.M{"rooms": room.ID}}
+
+	_, err := s.coll.UpdateByID(ctx, room.HotelID, update)
+	return err
+}
