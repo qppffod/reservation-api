b809b7bda93091653f2c010076b091b582712145
Artur <art67789@gmail.com>
2025-04-07 23:58:37 +0300

    Handler for booking a room

---
+++ api/room_handler.go
@@ -0,0 +1,80 @@
+package api
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/qppffod/reservation-api/db"
+	"github.com/qppffod/reservation-api/types"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type RoomHandler struct {
+	store *db.Store
+}
+
+func NewRoomHandler(store *db.Store) *RoomHandler {
+	return &RoomHandler{
+		store: store,
+	}
+}
+
+func (h *RoomHandler) HandleBookRoom(c *fiber.Ctx) error {
+	var params types.BookRoomParams
+	if err := c.BodyParser(&params); err != nil {
+		return err
+	}
+	if err := params.Validate(); err != nil {
+		return err
+	}
+	roomOID, err := primitive.ObjectIDFromHex(c.Params("id"))
+	if err != nil {
+		return err
+	}
+
+	user, ok := c.Context().UserValue("user").(*types.User)
+	if !ok {
+		return c.Status(http.StatusInternalServerError).JSON(GenericResponse{
+			Type: "error",
+			Msg:  "internal server error",
+		})
+	}
+
+	booking := types.Booking{
+		UserID:     user.ID,
+		RoomID:     roomOID,
+		NumPersons: params.NumPersons,
+		FromDate:   params.FromDate,
+		TillDate:   params.TillDate,
+	}
+
+	ok, err = h.isRoomAvailableForBooking(c.Context(), &booking)
+	if err != nil {
+		return err
+	}
+	if !ok {
+		return c.Status(http.StatusBadRequest).JSON(GenericResponse{
+			Type: "error",
+			Msg:  fmt.Sprintf("room %s already booked", c.Params("id")),
+		})
+	}
+
+	inserted, err := h.store.Booking.InsertBooking(c.Context(), &booking)
+	if err != nil {
+		return err
+	}
+
+	return c.JSON(inserted)
+}
+
+func (h *RoomHandler) isRoomAvailableForBooking(ctx context.Context, booking *types.Booking) (bool, error) {
+	bookings, err := h.store.Booking.GetBookings(ctx, booking)
+	if err != nil {
+		return false, err
+	}
+
+	ok := len(bookings) == 0
+	return ok, nil
+}
