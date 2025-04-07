3a3b5016d55f19b2eebea280bfe79e2fe46f1dd6
Artur <art67789@gmail.com>
2025-04-07 23:59:33 +0300

    add type booking

---
+++ types/booking.go
@@ -0,0 +1,31 @@
+package types
+
+import (
+	"fmt"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Booking struct {
+	ID         primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
+	UserID     primitive.ObjectID `bson:"userID,omitempty" json:"userID,omitempty"`
+	RoomID     primitive.ObjectID `bson:"roomID,omitempty" json:"roomID,omitempty"`
+	NumPersons int                `bson:"numPersons,omitempty" json:"numPersons,omitempty"`
+	FromDate   time.Time          `bson:"fromDate,omitempty" json:"fromDate,omitempty"`
+	TillDate   time.Time          `bson:"tillDate,omitempty" json:"tillDate,omitempty"`
+}
+
+type BookRoomParams struct {
+	FromDate   time.Time `json:"fromDate"`
+	TillDate   time.Time `json:"tillDate"`
+	NumPersons int       `json:"numPersons"`
+}
+
+func (p BookRoomParams) Validate() error {
+	now := time.Now()
+	if now.After(p.FromDate) || now.After(p.TillDate) {
+		return fmt.Errorf("cannot book a room in the past")
+	}
+	return nil
+}
