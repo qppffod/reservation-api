c878cfe4e69a05c7364b4e90f9fde3871996d5ee
Artur <art67789@gmail.com>
2025-04-07 07:55:41 +0300

    Add jwt middleware

--- api/jwt.go
+++ api/jwt.go
@@ -3,51 +3,71 @@ package api
 import (
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/golang-jwt/jwt/v5"
+	"github.com/qppffod/reservation-api/db"
 )
 
-func JWTAuthentication(c *fiber.Ctx) error {
-	headers, ok := c.GetReqHeaders()["X-Api-Token"]
-	if !ok {
-		fmt.Errorf("unauthorized")
+func JWTAuthentication(userStore db.UserStore) fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		headers, ok := c.GetReqHeaders()["X-Api-Token"]
+		if !ok {
+			fmt.Errorf("unauthorized")
+		}
+
+		if len(headers) < 1 {
+			fmt.Println("Invalid headers, headers array length should be at least 1")
+		}
+
+		token := headers[0]
+
+		claims, err := Parse(token)
+		if err != nil {
+			return err
+		}
+
+		expires := claims["expires"].(float64)
+		if time.Now().Unix() > int64(expires) {
+			return fmt.Errorf("token expired")
+		}
+
+		userID := claims["id"].(string)
+
+		user, err := userStore.GetUserByID(c.Context(), userID)
+		if err != nil {
+			return err
+		}
+
+		c.Context().SetUserValue("user", user)
+
+		return c.Next()
 	}
-
-	if len(headers) < 1 {
-		fmt.Println("Invalid headers, headers array length should be at least 1")
-	}
-
-	token := headers[0]
-
-	err := Parse(token)
-	if err != nil {
-		return err
-	}
-
-	fmt.Println(token)
-
-	return nil
 }
 
-func Parse(tokenString string) error {
+func Parse(tokenString string) (jwt.MapClaims, error) {
 	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			fmt.Printf("invalid signing method: %s", token.Header["alg"])
+			fmt.Println("invalid signing method ", token.Header["alg"])
 			return nil, fmt.Errorf("unauthorized")
 		}
 		secret := os.Getenv("JWT_SECRET")
 		return []byte(secret), nil
 	})
-
 	if err != nil {
 		fmt.Println("Failed to parse JWT Token:", err)
-		return fmt.Errorf("unauthorized")
+		return nil, fmt.Errorf("unauthorized")
 	}
 
-	if claims, ok := token.Claims.(jwt.MapClaims); ok {
-		fmt.Println(claims)
+	if !token.Valid {
+		return nil, fmt.Errorf("unauthorized")
 	}
 
-	return fmt.Errorf("unauthorized")
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok {
+		return nil, fmt.Errorf("unauthorized")
+	}
+
+	return claims, nil
 }
